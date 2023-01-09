f5e5f53ab67411719a3e3dcdbd5b8be99a46339f
Eric G <peaceandhealth@protonmail.com>
2023-01-08 18:40:50 -0800

    return a javascript object with desired values

--- script.js
+++ script.js
@@ -11,18 +11,27 @@ async function getWeather(event) {
 
         //Fetch the weather for that location
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${weatherLocation}&APPID=9fd2540735d4b8bf8b1d4439bfac44ef&units=imperial`;
+        console.log(url);
         let response = await fetch(url);
         let weatherData = await response.json();
 
         //Log the results to the console
         console.log(weatherData);
-        console.log(`Max temp: ${weatherData.main.temp_max}`);
-        console.log(`Min temp: ${weatherData.main.temp_min}`);
-        console.log(`Current temp: ${weatherData.main.temp}`);
-        console.log(`Feels like: ${weatherData.main.feels_like}`);
-        console.log(`Humidity: ${weatherData.main.humidity}`);
-        console.log(`Weather: ${weatherData.weather[0].description}`);
-        console.log(`Wind speed: ${weatherData.wind.speed}`);
+
+        //create an object with the required data
+        let todaysForecast = {
+            maxTemp: weatherData.main.temp_max,
+            minTemp: weatherData.main.temp_min,
+            currentTemp: weatherData.main.temp,
+            feelsLike: weatherData.main.feels_like,
+            humidity: weatherData.main.humidity,
+            weather: weatherData.weather[0].description,
+            windSpeed: weatherData.wind.speed,
+        }
+
+        console.log(todaysForecast);
+
+        return todaysForecast;
     } 
         
     catch (error){
